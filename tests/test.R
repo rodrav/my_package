b3592a31828f323f0e1e76d921e8aca59d41da8c
your_username <arayavalencia.rd@gmail.com>
2022-11-04 14:14:37 -0300

    actualizacion

--- tests/test.R
+++ tests/test.R
@@ -7,3 +7,5 @@ test_that('Test of make_filename function',{expect_that(make_filename(2015), is_
 test_that('Test of make_filename function',{expect_that(make_filename(2014), is_identical_to("accident_2014.csv.bz2"))})
 
 test_that('Test of make_filename function',{expect_that(make_filename(2015), is_identical_to("accident_2015.csv.bz2"))})
+
+#end test
